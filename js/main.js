a9c08c93ce2d5f219b0468a77686580e62328fcf
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-03-03 08:14:55 +0530

    Initial Work done

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+console.log(nav);
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }
+    else{
+        nav.classList.remove("scroll-on");
+    }
+};
+
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    });
+});
+
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1: -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("counter1", 0, 1288, 3000);
+    counter("counter2", 100, 5288, 2500);
+    counter("counter3", 0, 14438, 3000);
+    counter("counter4", 10, 7750, 3000);
+});
